fe3ccc303d664b8dfd467ea6a1bd44c7b6d47a1a
shoedogodo <46584549+shoedogodo@users.noreply.github.com>
2024-11-11 20:55:52 +0800

    Reset branch

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -1,52 +1,4 @@
-// index.js
-// const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
-
-// Page({
-//   data: {
-//     motto: 'Hello World',
-//     userInfo: {
-//       avatarUrl: defaultAvatarUrl,
-//       nickName: '',
-//     },
-//     hasUserInfo: false,
-//     canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-//     canIUseNicknameComp: wx.canIUse('input.type.nickname'),
-//   },
-//   bindViewTap() {
-//     wx.navigateTo({
-//       url: '../logs/logs'
-//     })
-//   },
-//   onChooseAvatar(e) {
-//     const { avatarUrl } = e.detail
-//     const { nickName } = this.data.userInfo
-//     this.setData({
-//       "userInfo.avatarUrl": avatarUrl,
-//       hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-//     })
-//   },
-//   onInputChange(e) {
-//     const nickName = e.detail.value
-//     const { avatarUrl } = this.data.userInfo
-//     this.setData({
-//       "userInfo.nickName": nickName,
-//       hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-//     })
-//   },
-//   getUserProfile(e) {
-//     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-//     wx.getUserProfile({
-//       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-//       success: (res) => {
-//         console.log(res)
-//         this.setData({
-//           userInfo: res.userInfo,
-//           hasUserInfo: true
-//         })
-//       }
-//     })
-//   },
-// })
+const utilsPath = require('../../utils/util.js'); // Path: frontend/utils/util.js
 
 Page({
     data: {
@@ -67,10 +19,31 @@ Page({
     },
   
     onLogin: function() {
+      console.log(utilsPath.server_URL);
+      
+
+
+      wx.request({
+        url: 'http://localhost:3000/api/data',  // URL to your backend API
+        method: 'GET',
+        success: (res) => {
+          console.log('Backend response:', res.data);
+          this.setData({
+            message: res.data.message
+          });
+        },
+        fail: (err) => {
+          console.error('Error:', err);
+        }
+      });
+
+    
+      /*
       // 直接跳转到首页
-      wx.navigateTo({
+      wx.navigateTo({ 
         url: '../home/home'
       });
+      */
     },
   
     onRegister: function() {
