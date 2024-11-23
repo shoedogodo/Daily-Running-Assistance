955ece5df42768bfd040f3c4412ae509271549b3
shoedogodo <46584549+shoedogodo@users.noreply.github.com>
2024-11-23 10:52:44 +0800

    Register and login on backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const User = require("../models/user.model");
 const router = express.Router();
-const {getUsers, getUser, createUser, updateUser, deleteUser} = require("../controllers/user.controller");
+const {getUsers, getUser, registerUser, updateUser, deleteUser, loginUser} = require("../controllers/user.controller");
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
-router.post('/', createUser);
+router.post('/', registerUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+router.post('/login', loginUser);
+
 
 module.exports = router;
