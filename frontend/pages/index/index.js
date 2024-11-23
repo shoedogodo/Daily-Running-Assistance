4bf3f147d9e03da48bea4d59c938da848ce6eead
shoedogodo <46584549+shoedogodo@users.noreply.github.com>
2024-11-23 10:55:10 +0800

    Added database

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -23,42 +23,6 @@ Page({
       wx.navigateTo({
         url: '../home/home',
       })
-
-      wx.request({
-        url: 'http://124.221.96.133:3000/api/login',
-        method: 'POST',
-        data: {
-          username: 'user',
-          password: 'pass'
-        },
-        success(res) {
-          // Store the token
-          wx.setStorageSync('token', res.data.token);
-        }
-      });
-
-      /*
-      wx.request({
-        url: 'http://localhost:3000/api/data',  // URL to your backend API
-        method: 'GET',
-        success: (res) => {
-          console.log('Backend response:', res.data);
-          this.setData({
-            message: res.data.message
-          });
-        },
-        fail: (err) => {
-          console.error('Error:', err);
-        }
-      });
-      */
-    
-      /*
-      // 直接跳转到首页
-      wx.navigateTo({ 
-        url: '../home/home'
-      });
-      */
     },
   
     onRegister: function() {
